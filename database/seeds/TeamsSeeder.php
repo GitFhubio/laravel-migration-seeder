6882c2831314ef73bff20a529a6774b2ec1d4b04
Fabio Petrone <fa610movic90@gmail.com>
2021-03-26 15:33:19 +0100

    .

--- database/seeds/TeamsSeeder.php
+++ database/seeds/TeamsSeeder.php
@@ -14,8 +14,8 @@ class TeamsSeeder extends Seeder
     {
         for ($i=0;$i<10;$i++) {
             $team = new Team();
-            $team->name=$faker->name();
-            $team->city=$faker->name();
+            $team->name=$faker->text(50);
+            $team->city=$faker->text(80);
             $team->colours=$faker->name();
             $team->stadium=$faker->name();
             $team->flag ='https://via.placeholder.com/90';
@@ -29,42 +29,26 @@ class TeamsSeeder extends Seeder
         // $team->flag='https://images-na.ssl-images-amazon.com/images/I/61P-S6gu0CL._AC_SL1500_.jpg';
         // $team->save();
 
-    //     $teams=[[
-    //  'name'=>'Roma',
+    //     $teams=[['name'=>'Roma',
     //  'colours'=>'giallorossi',
     //  'city'=>'Roma',
     //  'stadium'=>'Olimpico',
-    // //  'trophies'=>'18',
-    // //  'foundation_date'=>'1908-03-08',
-    //  'flag'=>'https://www.bandiere.it/uploads/2019-11-21/1200-0/citta-di-roma.jpg'
-    // //  'fans'=>'80000',
-    // //  'ridicolousness'=>'0'
-    // ],
-    //  [
-    //     'name'=>'Juve',
+    //  'flag'=>'https://www.bandiere.it/uploads/2019-11-21/1200-0/citta-di-roma.jpg'],
+    //  ['name'=>'Juve',
     //     'colours'=>'bianconeri',
     //     'city'=>'Torino',
     //     'stadium'=>'Juventus Stadium',
-    //     // 'trophies'=>'0',
-    //     // 'foundation_date'=>'1897-03-08',
     //     'flag'=>'https://images-na.ssl-images-amazon.com/images/I/61P-S6gu0CL._AC_SL1500_.jpg'
-    //     // 'fans'=>'90000',
-    //     // 'ridicolousness'=>'10'
     //     ]];
 
     //     foreach ($teams as $team) {
-    //         $team=new Team();
+    //         $team = new Team();
     //         $team->name=$team['name'];
     //         $team->colours=$team['colours'];
     //         $team->city=$team['city'];
     //         $team->stadium=$team['stadium'];
-    //         // $team->trophies=$team['trophies'];
-    //         // $team->foundation_date=$team['foundation_date'];
     //         $team->flag=$team['flag'];
-    //         // $team->fans=$team['fans'];
-    //         // $team->ridicolousness=$team['ridicolousness'];
     //         $team->save();
-    //         // }
     //     }
     }
 }
