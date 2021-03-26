ece306c8179d2345d928a6efb5b8bffa65391ce7
Fabio Petrone <fa610movic90@gmail.com>
2021-03-26 18:26:30 +0100

    note finali

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -7,10 +7,13 @@ use Illuminate\Http\Request;
 use App\Team;
 class ApiController extends Controller
 {
-public function __invoke(){
+  public function __invoke(){
+    $team= Team::orderBy('id','asc')->first();
+    // 1) sarebbe $team->toArray() ma il metodo json
+    // riesce a serializzare partendo anche da un oggetto
+    // 2)potrei prendere anche $teams=Team::all()
+    // e funzionerebbe allo stesso modo
+    return response()->json($team);
 
-$team= Team::orderBy('id','asc')->first();
-return response()->json($team);
-
-}
+  }
 }
