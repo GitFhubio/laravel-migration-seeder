9639c8df815e3e224344ca8c816f8078f690a551
Fabio Petrone <fa610movic90@gmail.com>
2021-03-26 15:45:17 +0100

    bug solved

--- database/seeds/TeamsSeeder.php
+++ database/seeds/TeamsSeeder.php
@@ -10,17 +10,17 @@ class TeamsSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
+    public function run()
     {
-        for ($i=0;$i<10;$i++) {
-            $team = new Team();
-            $team->name=$faker->text(50);
-            $team->city=$faker->text(80);
-            $team->colours=$faker->name();
-            $team->stadium=$faker->name();
-            $team->flag ='https://via.placeholder.com/90';
-            $team->save();
-        }
+        // for ($i=0;$i<10;$i++) {
+        //     $team = new Team();
+        //     $team->name=$faker->text(50);
+        //     $team->city=$faker->text(80);
+        //     $team->colours=$faker->name();
+        //     $team->stadium=$faker->name();
+        //     $team->flag ='https://via.placeholder.com/90';
+        //     $team->save();
+        // }
         // $team=new Team();
         // $team->name='Inter';
         // $team->colours='nerazzurri';
@@ -29,26 +29,26 @@ class TeamsSeeder extends Seeder
         // $team->flag='https://images-na.ssl-images-amazon.com/images/I/61P-S6gu0CL._AC_SL1500_.jpg';
         // $team->save();
 
-    //     $teams=[['name'=>'Roma',
-    //  'colours'=>'giallorossi',
-    //  'city'=>'Roma',
-    //  'stadium'=>'Olimpico',
-    //  'flag'=>'https://www.bandiere.it/uploads/2019-11-21/1200-0/citta-di-roma.jpg'],
-    //  ['name'=>'Juve',
-    //     'colours'=>'bianconeri',
-    //     'city'=>'Torino',
-    //     'stadium'=>'Juventus Stadium',
-    //     'flag'=>'https://images-na.ssl-images-amazon.com/images/I/61P-S6gu0CL._AC_SL1500_.jpg'
-    //     ]];
+        $teams=[["name"=>"Roma",
+     "colours"=>"giallorossi",
+     "city"=>"Roma",
+     "stadium"=>"Olimpico",
+     "flag"=>"https://www.bandiere.it/uploads/2019-11-21/1200-0/citta-di-roma.jpg"],
+     ["name"=>"Juve",
+        "colours"=>"bianconeri",
+        "city"=>"Torino",
+        "stadium"=>"Juventus Stadium",
+        "flag"=>"https://images-na.ssl-images-amazon.com/images/I/61P-S6gu0CL._AC_SL1500_.jpg"
+        ]];
 
-    //     foreach ($teams as $team) {
-    //         $team = new Team();
-    //         $team->name=$team['name'];
-    //         $team->colours=$team['colours'];
-    //         $team->city=$team['city'];
-    //         $team->stadium=$team['stadium'];
-    //         $team->flag=$team['flag'];
-    //         $team->save();
-    //     }
+        foreach ($teams as $team) {
+            $newteam = new Team();
+            $newteam->name=$team['name'];
+            $newteam->colours=$team['colours'];
+            $newteam->city=$team['city'];
+            $newteam->stadium=$team['stadium'];
+            $newteam->flag=$team['flag'];
+            $newteam->save();
+        }
     }
 }
